382f68f672fa37fefd6023ae1e2ee90f68edebd5
Akzaatz <akzaatz@protonmail.com>
2024-07-02 13:17:48 +0200

    font_03

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sass from "sass";
+import path from "path";
 
-// https://vitejs.dev/config/
+const basePath = process.env.VITE_BASE_URL || "/";
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/akzaatzPortfolio/" : "/",
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import "./src/utils/settings.scss";',
+        additionalData: `
+          @import "src/utils/_variables.scss";
+          $VITE_BASE_URL: '${basePath}';
+        `,
       },
     },
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import sass from "sass";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === "production" ? "/akzaatzPortfolio/" : "/",
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: '@import "./src/utils/settings.scss";',
+//       },
+//     },
+//   },
+// });
