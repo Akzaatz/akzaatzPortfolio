b1e0e028ff87d3a150d206fc71487fc1a028fa0b
Akzaatz <akzaatz@protonmail.com>
2024-09-09 12:17:36 +0200

    deploy: update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from "path";
 const basePath = process.env.VITE_BASE_URL || "/";
 
 export default defineConfig({
-  base: /akzaatzPortfolio/,
+  base: "/akzaatzPortfolio/",
+
   plugins: [react()],
   css: {
     preprocessorOptions: {
@@ -23,20 +24,3 @@ export default defineConfig({
     },
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import sass from "sass";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: process.env.NODE_ENV === "production" ? "/akzaatzPortfolio/" : "/",
-//   css: {
-//     preprocessorOptions: {
-//       scss: {
-//         additionalData: '@import "./src/utils/settings.scss";',
-//       },
-//     },
-//   },
-// });
