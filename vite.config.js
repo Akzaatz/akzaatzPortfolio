f05dcb6344e2d1b4b67cc44d8f13e95c6f20620e
Akzaatz <akzaatz@protonmail.com>
2025-02-13 16:49:21 +0100

    fix: update SASS imports to use @use instead of @import

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const basePath = process.env.VITE_BASE_URL || "/";
-
 export default defineConfig({
-  base: "/akzaatzPortfolio/",
-  // base: "/",
+  base: "/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `
-          @import "src/utils/_variables.scss";
-          $VITE_BASE_URL: '${basePath}';
-        `,
+        additionalData: `$VITE_BASE_URL: '/';`,
+        includePaths: [path.resolve(__dirname, "./src")],
       },
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
   resolve: {
     alias: {
@@ -24,43 +23,3 @@ export default defineConfig({
     },
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// const basePath = process.env.VITE_BASE_URL || "/";
-
-// export default defineConfig({
-//   base: "/", // Correct pour un déploiement sur domaine racine
-//   plugins: [react()],
-//   build: {
-//     outDir: "dist",
-//     assetsDir: "assets",
-//     // Optimisations pour la production
-//     minify: "terser",
-//     sourcemap: false,
-//     // Configuration pour améliorer le chargement
-//     rollupOptions: {clear
-//         manualChunks: {
-//           vendor: ["react", "react-dom"],
-//         },
-//       },
-//     },
-//   },
-//   css: {
-//     preprocessorOptions: {
-//       scss: {
-//         additionalData: `
-//           @import "src/utils/_variables.scss";
-//           $VITE_BASE_URL: '${basePath}';
-//         `,
-//       },
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
