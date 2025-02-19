dba4a96bdd6c578437e70559ed82d88795ada1f7
Akzaatz <akzaatz@protonmail.com>
2025-02-19 14:38:32 +0100

    actualisation vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,34 +5,28 @@ import path from "path";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
   },
-  preview: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "akzaatz.com",
-      ".akzaatz.com",
-      ".sslip.io",
-    ],
-  },
+  publicDir: "public",
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `$VITE_BASE_URL: '/';`,
-        includePaths: [path.resolve(__dirname, "./src")],
       },
     },
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
   },
   resolve: {
     alias: {
